29fc5149e49b3dea9c2c7e5f488aae8dfeecb758
CamelGamer123 <kylepannan@gmail.com>
2024-05-08 12:48:01 +1000

    Modified a whole bunch of __init__.py's

--- server/internals/routes/__init__.py
+++ server/internals/routes/__init__.py
@@ -4,8 +4,10 @@ Initializes the routes for the server.
 
 from .info import infoBlueprint
 from .games import gamesBlueprint
+from .tests import testsBlueprint
 
 __all__ = [
     "infoBlueprint",
-    "gamesBlueprint"
+    "gamesBlueprint",
+    "testsBlueprint"
 ]
---
+++ server/internals/wrapper/__init__.py
@@ -0,0 +1,8 @@
+"""
+Initializes the wrapper package.
+"""
+
+from .api import API
+from .response import Response
+from .types import *
+from .handlers import *
