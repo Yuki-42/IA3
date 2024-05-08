6db2d26741fa791579d7b1b11f395aa8db01cf2d
Yuki <kylepannan@gmail.com>
2024-05-08 21:39:25 +1000

    Added api blueprint

--- server/internals/routes/__init__.py
+++ server/internals/routes/__init__.py
@@ -5,9 +5,11 @@ Initializes the routes for the server.
 from .info import infoBlueprint
 from .games import gamesBlueprint
 from .tests import testsBlueprint
+from .api import apiBlueprint
 
 __all__ = [
     "infoBlueprint",
     "gamesBlueprint",
-    "testsBlueprint"
+    "testsBlueprint",
+    "apiBlueprint"
 ]
