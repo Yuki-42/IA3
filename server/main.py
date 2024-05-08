dbcf976fdb5af4fac4d725cb12d2847b73ce7528
CamelGamer123 <kylepannan@gmail.com>
2024-05-08 12:22:38 +1000

    Additions to main to allow for DI

--- server/main.py
+++ server/main.py
@@ -6,13 +6,19 @@ Main file for IA3 Project.
 
 # Third Party Imports
 from flask import Flask
+from flask_injector import FlaskInjector
+from injector import Binder, singleton
 
 # Local Imports
 from internals.config import Config
-from server.internals.routes import infoBlueprint, gamesBlueprint
+from internals.routes import infoBlueprint, gamesBlueprint
+from internals.wrapper.api import API
 
 # Constants
 config: Config = Config()
+
+# Connect to the API
+api: API = API(config)
 
 # Create the Flask app
 app = Flask(__name__, static_folder="static", template_folder="templates")
@@ -20,6 +26,21 @@ app = Flask(__name__, static_folder="static", template_folder="templates")
 # Add routes
 app.register_blueprint(infoBlueprint)
 app.register_blueprint(gamesBlueprint)
+
+
+def configureDependencies(binder: Binder):
+    """
+    Configures the dependencies for the app.
+
+    Args:
+        binder (Binder): The binder to use.
+    """
+    binder.bind(Config, config, scope=singleton)
+    binder.bind(API, api, scope=singleton)
+
+
+# Add dependencies
+FlaskInjector(app=app, modules=[configureDependencies])
 
 # Run the app
 if __name__ == "__main__":
