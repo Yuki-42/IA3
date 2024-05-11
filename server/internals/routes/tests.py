94c6b4401c297ab7156af99abcefabafc2cdea09
Yuki <kylepannan@gmail.com>
2024-05-11 21:37:39 +1000

    More tests work

--- server/internals/routes/tests.py
+++ server/internals/routes/tests.py
@@ -10,6 +10,7 @@ from flask_injector import inject
 
 # Internal Imports
 from ..helpers import renderTemplate
+from ..wrapper import API, Response
 
 # Constants
 testsBlueprint: Blueprint = Blueprint("tests", __name__, url_prefix="/tests")
@@ -28,7 +29,6 @@ def index() -> str:
 
 
 @testsBlueprint.get("/creator")
-@inject
 def creator() -> str:
     """
     The creator tests page.
@@ -36,4 +36,64 @@ def creator() -> str:
     Returns:
         str: The rendered creator tests page.
     """
-    return renderTemplate("tests/creator.html")
+    return renderTemplate("tests/creator/index.html")
+
+
+@testsBlueprint.get("/creator/<string:type>")
+@inject
+def creatorClass(
+        api: API,
+        testType: str = None,
+        id: str = None,
+        page: int = None,
+        pageSize: int = None
+) -> str:
+    """
+    The creator class tests page.
+
+    Args:
+        api (API): The API wrapper.
+        testType (str): The type of test to run.
+        id (str): The id of the creator.
+        page (int): The page number.
+        pageSize (int): The number of items per page.
+
+    Returns:
+        str: The rendered creator class tests page.
+    """
+
+    if testType not in ["list", "details"]:
+        return renderTemplate("tests/creator/index.html", error="Invalid test type.")
+
+    # If the test type is list, check if the page and pageSize are valid.
+    if testType == "list":
+        if page is None or pageSize is None:
+            return renderTemplate(
+                "tests/creator/index.html",
+                error="<code>page</code> and <code>pageSize</code> are required."
+            )
+
+        if page < 1 or pageSize < 1:
+            return renderTemplate(
+                "tests/creator/index.html",
+                error="<code>page</code> and <code>pageSize</code> must be greater than 0."
+            )
+
+        if pageSize >= 25:
+            return renderTemplate(
+                "tests/creator/index.html",
+                error="<code>pageSize</code> must be less than or equal to 25."
+            )
+
+        # Get the creators from the API.
+        response: Response = api.creator.getCreators(page=page, pageSize=pageSize)
+        return renderTemplate("tests/creator/list.html", creators=response.results)
+
+    # Test type is details.
+    if id is None:
+        return renderTemplate("tests/creator/index.html", error="<code>id</code> is required.")
+
+    # Get the creator from the API.
+    response: Response = api.creator.getCreator(id=id)
+    return renderTemplate("tests/creator/details.html", creator=response)
+
--- server/static/js/tests/creator.js
+++ server/static/js/tests/creator.js
@@ -1,4 +1,4 @@
-async function getAll() {
+async function getList() {
     // Get parameters
     let page = document.getElementById("all-page").value;
     let pageSize = document.getElementById("all-page-size").value;
@@ -25,4 +25,21 @@ async function getDetails() {
     const tree = jsonview.create(data);
     jsonview.render(tree, document.getElementById("get-details-response"));
     tree.id = "get-details-response-data";
+}
+
+async function getListClass() {
+    // Get parameters
+    let page = document.getElementById("all-page").value;
+    let pageSize = document.getElementById("all-page-size").value;
+
+    // Send the user to the /tests/creator/list page
+    window.location.href = "/tests/creator/list?page=" + page + "&pageSize=" + pageSize;
+}
+
+async function getDetailsClass() {
+    // Get parameters
+    let id = document.getElementById("details-id-class").value;
+
+    // Send the user to the /tests/creator/details page
+    window.location.href = "/tests/creator/details?id=" + id;
 }
