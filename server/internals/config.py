33988ba95bee74417c5511b0ec69a6dc4c75d6c1
CamelGamer123 <kylepannan@gmail.com>
2024-05-14 11:18:29 +1000

    Added some more classes to config

--- server/internals/config.py
+++ server/internals/config.py
@@ -9,6 +9,48 @@ from logging import INFO, DEBUG, WARNING, ERROR
 from dotenv import load_dotenv as loadEnv
 
 
+class Owner:
+    """
+    Contains owner related config data.
+    """
+    name: str
+    contact: str
+
+    def __init__(
+            self
+    ) -> None:
+        """
+        Initializes the Owner object.
+
+        Returns:
+            None
+        """
+        self.name = environ.get("SERVER_OWNER_NAME")
+        self.contact = environ.get("SERVER_OWNER_CONTACT")
+
+
+class SSL:
+    """
+    Contains SSL related config data.
+    """
+    cert: str
+    key: str
+    enabled: bool
+
+    def __init__(
+            self
+    ) -> None:
+        """
+        Initializes the SSL object.
+
+        Returns:
+            None
+        """
+        self.cert = environ.get("SERVER_SSL_CERT")
+        self.key = environ.get("SERVER_SSL_KEY")
+        self.enabled = environ.get("SERVER_SSL_ENABLED") == "True"
+
+
 class Server:
     """
     Contains server related config data.
@@ -18,9 +60,8 @@ class Server:
     debug: bool
     username: str
     password: str
-    cert: str
-    key: str
-    ssl: bool
+    ssl: SSL
+    owner: Owner
 
     def __init__(
             self
@@ -36,10 +77,9 @@ class Server:
         self.debug = environ.get("SERVER_DEBUG") == "True"
         self.username = environ.get("SERVER_USER")
         self.password = environ.get("SERVER_PASSWORD")
-        self.cert = environ.get("SERVER_CERT")
-        self.key = environ.get("SERVER_KEY")
-        self.ssl = environ.get("SERVER_SSL") == "True"
         self.defaultTheme = environ.get("SERVER_DEFAULT_THEME")
+        self.ssl = SSL()
+        self.owner = Owner()
 
 
 class Logging:
--- server/wsgi.py
+++ server/wsgi.py
@@ -9,5 +9,5 @@ if __name__ == "__main__":
         host=config.server.host,
         port=config.server.port,
         debug=config.server.debug,
-        ssl_context=(config.server.cert, config.server.key) if config.server.ssl else None
+        ssl_context=(config.server.ssl.cert, config.server.ssl.key) if config.server.ssl.enabled else None
     )
