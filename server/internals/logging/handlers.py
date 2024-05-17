c464f153d7306d0cfcd12ea8a4980deeca778935
Yuki <kylepannan@gmail.com>
2024-05-18 03:01:03 +1000

    Oh my. Sweet jesus. That took a lot of debugging for the dumbest error ever

--- server/internals/logging/handlers.py
+++ server/internals/logging/handlers.py
@@ -88,204 +88,189 @@ class DatabaseLogHandler(Handler):
         Returns:
             None
         """
-        # # Log where the record is coming from
-        # # print(f"Logging record from {record.filename}:{record.lineno}")
-        # print(has_request_context())
-        # # Testing code
-        # if has_request_context() and not self.includeRequest:
-        #     raise ValueError("Request information was logged without a request context.")
-#
-        # if ("request" in record.message or "response" in record.message) and (not has_request_context() or not self.includeRequest):
-        #     raise ValueError("Request or response information was logged without a request context.")
+
+        # Create a cursor for the whole function
+        cursor: RealDictCursor = self.connection.cursor(cursor_factory=RealDictCursor)
 
         # Add the record to the database and get the id
-        recordId: str = self._logRecord(record)
+        recordId: str = self._logRecord(cursor, record)  # Why is this not being executed when request context is present
 
         if not has_request_context() or not self.includeRequest:
-            print("ignoring request")
             return
 
         # Check what state the request is in
-        if g.completed:
-            self._logResponse(recordId, g.response)
+        if not g.completed:
+            self._logRequest(cursor, recordId)
+
         else:
-            self._logRequest(recordId, request)
+            self._logResponse(cursor, g.response)
 
         self.connection.commit()
 
+    @staticmethod
     def _logRecord(
-            self,
+            cursor: RealDictCursor,
             record: LogRecord
     ) -> str:
         """
         Logs the record to the database.
 
         Args:
+            cursor (RealDictCursor): The cursor to use.
             record (LogRecord): The record to log.
 
         Returns:
             str: The id of the record.
         """
-        with self.connection.cursor(cursor_factory=RealDictCursor) as cursor:
-            cursor: RealDictCursor
-            cursor.execute(
-                """
-                INSERT INTO ia3.program_logs (
-                    timestamp,
-                    level,
-                    filename,
-                    funcname,
-                    lineno,
-                    message,
-                    module,
-                    name,
-                    pathname,
-                    process,
-                    process_name,
-                    thread,
-                    thread_name
-                ) VALUES (
-                    %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s
-                ) RETURNING id;
-                """,
-                (
-                    datetime.fromtimestamp(record.created),
-                    record.levelno,
-                    record.filename,
-                    record.funcName,
-                    record.lineno,
-                    record.msg,
-                    record.module,
-                    record.name,
-                    record.pathname,
-                    record.process,
-                    record.processName,
-                    record.thread,
-                    record.threadName
-                )
+        cursor.execute(
+            """
+            INSERT INTO ia3.program_logs (
+                timestamp,
+                level,
+                filename,
+                funcname,
+                lineno,
+                message,
+                module,
+                name,
+                pathname,
+                process,
+                process_name,
+                thread,
+                thread_name
+            ) VALUES (
+                %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s
+            ) RETURNING id;
+            """,
+            (
+                datetime.fromtimestamp(record.created),
+                record.levelno,
+                record.filename,
+                record.funcName,
+                record.lineno,
+                record.msg,
+                record.module,
+                record.name,
+                record.pathname,
+                record.process,
+                record.processName,
+                record.thread,
+                record.threadName
             )
-            self.connection.commit()
+        )
 
-            return cursor.fetchone()["id"]
+        return cursor.fetchone()["id"]
 
+    @staticmethod
     def _logRequest(
-            self,
-            recordId: str,
-            _request: Request
+            cursor: RealDictCursor,
+            recordId: str
     ) -> None:
         """
         Logs the request information to the database.
 
         Args:
-            recordId (str): The id of the record to log the request information to. Used to link the request information
-                to the log record.
-            _request (request): The request object to log.
+            cursor (RealDictCursor): The cursor to use.
+            recordId (str): The id of the record.
 
         Returns:
             None
         """
         # Check if the X-Forwarded-For header is present
-        if "X-Forwarded-For" in _request.headers:
-            remoteAddr = _request.headers["X-Forwarded-For"]
+        if "X-Forwarded-For" in request.headers:
+            remoteAddr = request.headers["X-Forwarded-For"]
         else:
-            remoteAddr = _request.remote_addr
+            remoteAddr = request.remote_addr
 
-        with self.connection.cursor(cursor_factory=RealDictCursor) as cursor:
-            cursor.execute(
-                """
-                INSERT INTO ia3.requests (
-                    id, 
-                    log_id, 
-                    view_args, 
-                    routing_exception, 
-                    endpoint, 
-                    blueprint, 
-                    blueprints,
-                    accept_languages, 
-                    accept_mimetypes, 
-                    access_route, 
-                    args, 
-                    "authorization", 
-                    base_url, 
-                    cookies, 
-                    full_path, 
-                    host, 
-                    host_url, 
-                    url, 
-                    method, 
-                    headers, 
-                    remote_addr
-                ) VALUES (
-                    %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s
-                )
-                """,
-                (
-                    recordId,
-                    str(g.uuid),
-                    Json(_request.view_args) if _request.view_args is not None else None,
-                    _request.routing_exception.__str__() if _request.routing_exception is not None else None,
-                    _request.endpoint if _request.endpoint is not None else None,
-                    _request.blueprint if _request.blueprint is not None else None,
-                    _request.blueprints if _request.blueprints is not None else None,
-                    _request.accept_languages.__str__() if _request.accept_languages is not None else None,
-                    _request.accept_mimetypes.__str__() if _request.accept_mimetypes is not None else None,
-                    _request.access_route if _request.access_route is not None else None,
-                    Json(_request.args.to_dict()) if _request.args is not None else None,
-                    _request.authorization.__str__() if _request.authorization is not None else None,
-                    _request.base_url if _request.base_url is not None else None,
-                    Json(_request.cookies.to_dict()) if _request.cookies is not None else None,
-                    _request.full_path if _request.full_path is not None else None,
-                    _request.host if _request.host is not None else None,
-                    _request.host_url if _request.host_url is not None else None,
-                    _request.url if _request.url is not None else None,
-                    _request.method if _request.method is not None else None,
-                    _request.headers.__str__() if _request.headers is not None else None,
-                    remoteAddr  # This will never be None
-                )
+        cursor.execute(
+            """
+            INSERT INTO ia3.requests (
+                id, 
+                log_id, 
+                view_args, 
+                routing_exception, 
+                endpoint, 
+                blueprint, 
+                blueprints,
+                accept_languages, 
+                accept_mimetypes, 
+                access_route, 
+                args, 
+                "authorization", 
+                base_url, 
+                cookies, 
+                full_path, 
+                host, 
+                host_url, 
+                url, 
+                method, 
+                headers, 
+                remote_addr
+            ) VALUES (
+                %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s
             )
-            self.connection.commit()
+            """,
+            (
+                str(g.uuid),
+                recordId,
+                Json(request.view_args) if request.view_args is not None else None,
+                request.routing_exception.__str__() if request.routing_exception is not None else None,
+                request.endpoint if request.endpoint is not None else None,
+                request.blueprint if request.blueprint is not None else None,
+                request.blueprints if request.blueprints is not None else None,
+                request.accept_languages.__str__() if request.accept_languages is not None else None,
+                request.accept_mimetypes.__str__() if request.accept_mimetypes is not None else None,
+                request.access_route if request.access_route is not None else None,
+                Json(request.args.to_dict()) if request.args is not None else None,
+                request.authorization.__str__() if request.authorization is not None else None,
+                request.base_url if request.base_url is not None else None,
+                Json(request.cookies.to_dict()) if request.cookies is not None else None,
+                request.full_path if request.full_path is not None else None,
+                request.host if request.host is not None else None,
+                request.host_url if request.host_url is not None else None,
+                request.url if request.url is not None else None,
+                request.method if request.method is not None else None,
+                request.headers.__str__() if request.headers is not None else None,
+                remoteAddr  # This will never be None
+            )
+        )
 
+    @staticmethod
     def _logResponse(
-            self,
-            recordId: str,
+            cursor: RealDictCursor,
             response: Response
     ) -> None:
         """
         Logs the response information to the database.
 
         Args:
-            recordId (str): The id of the record to log the response information to. Used to link the response information
-                to the log record.
+            cursor (RealDictCursor): The cursor to use.
             response (Response): The response object to log.
 
         Returns:
             None
         """
-        with self.connection.cursor(cursor_factory=RealDictCursor) as cursor:
-            cursor.execute(
-                """
-                INSERT INTO ia3.responses (
-                    id, 
-                    request_id, 
-                    expires, 
-                    location, 
-                    status, 
-                    status_code, 
-                    headers, 
-                    response
-                ) VALUES  (
-                    %s, %s, %s, %s, %s, %s, %s, %s
-                );
-                """,
-                (
-                    recordId,
-                    str(g.uuid),
-                    response.expires if response.expires is not None else None,
-                    response.location if response.location is not None else None,
-                    response.status if response.status is not None else None,
-                    response.status_code if response.status_code is not None else None,
-                    response.headers.__str__() if response.headers is not None else None,
-                    response.response.__str__() if response.response is not None else None
-                )
+        cursor.execute(
+            """
+            INSERT INTO ia3.responses (
+                request_id, 
+                expires, 
+                location, 
+                status, 
+                status_code, 
+                headers, 
+                response
+            ) VALUES  (
+                %s, %s, %s, %s, %s, %s, %s
+            );
+            """,
+            (
+                str(g.uuid),
+                response.expires if response.expires is not None else None,
+                response.location if response.location is not None else None,
+                response.status if response.status is not None else None,
+                response.status_code if response.status_code is not None else None,
+                response.headers.__str__() if response.headers is not None else None,
+                response.response.__str__() if response.response is not None else None
             )
-            self.connection.commit()
+        )
--- server/internals/logging/schema.sql
+++ server/internals/logging/schema.sql
@@ -4,13 +4,16 @@ Initialise logs database. More detailed database layout can be found in [docs/da
 LANG=PGSQL
 */
 
+/* Check what database is currently selected */
+SELECT CURRENT_DATABASE();
+
 /* Activate the UUID Extension if it is not already active */
 CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 
 /* Create tables */
 CREATE TABLE IF NOT EXISTS ia3.program_logs
 (
-    id           uuid      NOT NULL PRIMARY KEY DEFAULT uuid_generate_v4(),
+    id           uuid      NOT NULL PRIMARY KEY DEFAULT ia3.uuid_generate_v4(),
     timestamp    TIMESTAMP NOT NULL,
     level        INT       NOT NULL,
     filename     TEXT      NOT NULL,
@@ -28,9 +31,9 @@ CREATE TABLE IF NOT EXISTS ia3.program_logs
 
 CREATE TABLE IF NOT EXISTS ia3.requests
 (
-    id                VARCHAR(36) NOT NULL PRIMARY KEY,
+    id                uuid NOT NULL PRIMARY KEY,
     log_id            uuid NOT NULL,
-    view_args         JSONB,
+    view_args         jsonb,
     routing_exception TEXT,
     endpoint          TEXT,
     blueprint         TEXT,
@@ -38,22 +41,22 @@ CREATE TABLE IF NOT EXISTS ia3.requests
     accept_languages  TEXT,
     accept_mimetypes  TEXT,
     access_route      TEXT[],
-    args              JSONB,
+    args              jsonb,
     "authorization"   TEXT,
     base_url          TEXT,
-    cookies           JSONB,
+    cookies           jsonb,
     full_path         TEXT,
     host              TEXT,
     host_url          TEXT,
     url               TEXT,
     method            TEXT,
     headers           TEXT,
-    remote_addr       TEXT        NOT NULL
+    remote_addr       TEXT NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS ia3.responses
 (
-    id          uuid NOT NULL PRIMARY KEY,
+    id          uuid NOT NULL PRIMARY KEY DEFAULT ia3.uuid_generate_v4(),
     request_id  uuid NOT NULL,
     expires     TIMESTAMP,
     location    TEXT,
@@ -64,23 +67,26 @@ CREATE TABLE IF NOT EXISTS ia3.responses
 );
 
 /* Create indexes that don't exist */
-CREATE INDEX program_logs_timestamp ON ia3.program_logs (timestamp);
-CREATE INDEX program_logs_level ON ia3.program_logs (level);
-CREATE INDEX program_logs_funcname ON ia3.program_logs (funcname);
-CREATE INDEX program_logs_module ON ia3.program_logs (module);
-CREATE INDEX program_logs_name ON ia3.program_logs (name);
+CREATE INDEX IF NOT EXISTS program_logs_timestamp ON ia3.program_logs (timestamp);
+CREATE INDEX IF NOT EXISTS program_logs_level ON ia3.program_logs (level);
+CREATE INDEX IF NOT EXISTS program_logs_funcname ON ia3.program_logs (funcname);
+CREATE INDEX IF NOT EXISTS program_logs_module ON ia3.program_logs (module);
+CREATE INDEX IF NOT EXISTS program_logs_name ON ia3.program_logs (name);
 
-CREATE INDEX requests_log_id ON ia3.requests (log_id);
-CREATE INDEX requests_endpoint ON ia3.requests (endpoint);
-CREATE INDEX requests_blueprint ON ia3.requests (blueprint);
-CREATE INDEX requests_method ON ia3.requests (method);
-CREATE INDEX requests_remote_addr ON ia3.requests (remote_addr);
+CREATE INDEX IF NOT EXISTS requests_log_id ON ia3.requests (log_id);
+CREATE INDEX IF NOT EXISTS requests_endpoint ON ia3.requests (endpoint);
+CREATE INDEX IF NOT EXISTS requests_blueprint ON ia3.requests (blueprint);
+CREATE INDEX IF NOT EXISTS requests_method ON ia3.requests (method);
+CREATE INDEX IF NOT EXISTS requests_remote_addr ON ia3.requests (remote_addr);
 
-CREATE INDEX responses_request_id ON ia3.responses (request_id);
-CREATE INDEX responses_status_code ON ia3.responses (status_code);
+CREATE INDEX IF NOT EXISTS responses_request_id ON ia3.responses (request_id);
+CREATE INDEX IF NOT EXISTS responses_status_code ON ia3.responses (status_code);
 
 /* Create foreign keys */
-/* YEAH NO. THIS FOR SOME REASON BREAKS THE DATABASE. */
+ALTER TABLE ia3.requests
+    ADD FOREIGN KEY (log_id) REFERENCES ia3.program_logs (id);
+ALTER TABLE ia3.responses
+    ADD FOREIGN KEY (request_id) REFERENCES ia3.requests (id);
 
 /* Give all permissions to the loghandler user */
 GRANT ALL ON ALL TABLES IN SCHEMA ia3 TO loghandler;
@@ -92,26 +98,23 @@ DROP VIEW IF EXISTS ia3.simple_responses;
 
 /* View joins program_logs, requests, and responses. More columns will be manually added */
 CREATE VIEW ia3.web_logs(timestamp, method, remote_addr, path, status) AS
-SELECT
-    program_logs.timestamp,
-    requests.method,
-    requests.remote_addr,
-    requests.full_path,
-    responses.status_code
+SELECT program_logs.timestamp,
+       requests.method,
+       requests.remote_addr,
+       requests.full_path,
+       responses.status_code
 FROM ia3.program_logs
-JOIN
-    ia3.requests ON program_logs.id = requests.log_id
-JOIN
-    ia3.responses ON requests.id = responses.request_id;
+         JOIN
+     ia3.requests ON program_logs.id = requests.log_id
+         JOIN
+     ia3.responses ON requests.id = responses.request_id;
 
+COMMENT ON VIEW ia3.web_logs IS 'Used for viewing unified web logs.';
 
+ALTER VIEW ia3.web_logs
+    OWNER TO loghandler;
 
-comment on view ia3.web_logs is 'Used for viewing unified web logs.';
-
-alter view ia3.web_logs
-    owner to loghandler;
-
-create view ia3.simple_requests(method, remote_addr, full_path, cookies, args, view_args, id, log_id) as
+CREATE VIEW ia3.simple_requests(method, remote_addr, full_path, cookies, args, view_args, id, log_id) AS
 SELECT requests.method,
        requests.remote_addr,
        requests.full_path,
@@ -122,13 +125,13 @@ SELECT requests.method,
        requests.log_id
 FROM ia3.requests;
 
-comment on view ia3.simple_requests is 'Used to view requests in a more logical order and format.';
+COMMENT ON VIEW ia3.simple_requests IS 'Used to view requests in a more logical order and format.';
 
-alter table ia3.simple_requests
-    owner to loghandler;
+ALTER TABLE ia3.simple_requests
+    OWNER TO loghandler;
 
 
-create view ia3.simple_responses(status, status_code, request_id, headers, response) as
+CREATE VIEW ia3.simple_responses(status, status_code, request_id, headers, response) AS
 SELECT response.status,
        response.status_code,
        response.request_id,
@@ -136,9 +139,9 @@ SELECT response.status,
        response.response
 FROM ia3.responses response;
 
-comment on view ia3.simple_responses is 'Used to view responses in a more logical order and format.';
+COMMENT ON VIEW ia3.simple_responses IS 'Used to view responses in a more logical order and format.';
 
-alter table ia3.simple_responses
-    owner to loghandler;
+ALTER TABLE ia3.simple_responses
+    OWNER TO loghandler;
 
 
