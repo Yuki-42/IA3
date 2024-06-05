685b793295ae18dd20f3d03a55f19ea32bc2fb25
Yuki-42 <kylepannan@gmail.com>
2024-06-05 11:16:37 +1000

    Started work on games tests

---
+++ server/static/js/tests/game.js
@@ -0,0 +1,9 @@
+async function getList(){
+    // Get parameters
+    let page = document.getElementById("list-page").value;
+    let pageSize = document.getElementById("list-page-size").value;
+    let search = document.getElementById("list-search").value;
+    let searchPrecise = document.getElementById("list-search-precise").checked;
+    let searchExact = document.getElementById("list-search-exact").checked;
+
+}
